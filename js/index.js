4fca866be2217ac364a23ad5039b558da9591172
Anja Klaßen <anja.klassen@posteo.de>
2024-01-20 23:47:14 +0100

    add solution

--- js/index.js
+++ js/index.js
@@ -41,6 +41,9 @@ fetchDataAndRender();
 
 // --v-- your code below this line --v--
 
-function fetchDataAndRender() {
-  fetch(); // ?
+async function fetchDataAndRender() {
+  const response = await fetch("https://swapi.dev/api/people");
+  const data = await response.json();
+  data.results.forEach((character) => renderElement(Card(character)));
+  return data;
 }
